af4a0a1799b3d9f6c8e1ac49af1360c12cab35b6
cuzai <cuzai@naver.com>
2019-04-27 19:16:26 +0900

    ..

--- section4/new.py
+++ section4/new.py
@@ -15,3 +15,36 @@ tfilename = 'C:/Users/cuzai/Desktop/Web_Crawling/pythonStudy/section4/test.txt'
 data1 = 77
 data2 ="Hello World"
 data3 = ['car', 'apple', 'house']
+
+# 바이너리 쓰기
+with open(bfilename, 'wb') as f :
+    pickle.dump(data1, f)   #dumps는 객체를 바이너리화 할 때, dump는 그냥 텍스트를 바이너리화(직렬화) 할 때
+    pickle.dump(data2, f)
+    pickle.dump(data3, f)
+
+# 텍스트 쓰기
+
+with open(tfilename, 'wt') as f :
+    f.write(str(data1))
+    f.write('\n')
+    f.write(data2)
+    f.write('\n')
+    f.writelines('\n'.join(data3))  # 리스트 그 자체를 바로 write 할 수는 없음. 그래서 파이썬 writelines 메소드를 통해
+                                    # 리스트를 순회하며 쓸 수 있도록 해 주고, 파이썬 자체 함수인 join을 통해 ??해 준다.
+                                    #좀 더 찾아볼 것
+
+# 바이너리 읽기
+with open(bfilename, 'rb') as f :
+    b = pickle.load(f)
+    print(type(b), 'binary read1 : ', b)
+    b = pickle.load(f)
+    print(type(b), 'binary read2 : ', b)
+    b = pickle.load(f)
+    print(type(b), 'binary read3 : ', b)
+
+
+
+# 텍스트 읽기
+with open(tfilename, 'rt') as f :
+    for i, line in enumerate(f, 1) :
+        print(type(line), 'text read', str(i) + " " +line, end = "")
