a2df8a95db3ca0bfd10e5746120e192ccb430e94
cuzai <cuzai@naver.com>
2019-05-04 15:26:36 +0900

    ..

--- section4/numpyTest.py
+++ section4/numpyTest.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+#랜덤으로 DataFrame 생성
+df = pd.DataFrame(np.random.randint(0, 100, size = (100, 4)), columns = list('ABCD'))   #colums = ['one', 'two', 'three', 'four']도 가능
+print(df)
+
+df = pd.DataFrame(np.random.randn(100, 4), columns = ['one', 'two', 'three', 'four'])
+print(df)
+
+df.to_csv('result_s2.csv', index = False, header = False)
