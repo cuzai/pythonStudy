e339b30db99926c10eea56c2a3c1bd174b2d9f37
cuzai <cuzai@naver.com>
2019-05-04 17:38:36 +0900

    ..

--- section4/dataframeTest.py
+++ section4/dataframeTest.py
@@ -6,6 +6,13 @@ r_data = {'City' : ['서울', '대전', '대구', '부산'], 'Total1' : [55000, 
 i_data = ['One', 'Two', 'Three', 'Four']
 
 d_frame = DataFrame(r_data, index = i_data)
-print(d_frame)
-print(d_frame.index)
-print(d_frame.values)
+# print(d_frame)
+# print(d_frame.index)
+# print(d_frame.values)
+# print(d_frame['City'])  # 열 기준으로 가져오기. type는 series
+# print(d_frame.ix['One'])    # 행 기준으로 가져오기. type는 series
+
+# 값 순회
+for e in d_frame.values :
+    for n, i in enumerate(e) :
+        print(n, i)
